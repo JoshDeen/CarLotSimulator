3ca60c704f62814e47b452ff824e1037f9743dab
Joshua Wilson <joshdeenwilson@gmail.com>
2024-06-13 15:42:38 -0400

    Updated errors and added Static Exercise

--- CarLotSimulatorApp/CarLotSimulator/Car.cs
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -7,30 +7,28 @@ using System.Xml.Schema;
 
 namespace CarLotSimulator
 {
-    
-}
-public class Car
-{
-    public int Year { get; set; }
-    public string Make { get; set; }
-    public string Model { get; set; }
-    public string EngineNoise { get; set; }
-    public string HonkNoise { get; set; }
-    public bool IsDriveable { get; set; }
-
-    public void MakeEngineNoise(string noise)
+    public class Car
     {
-        Console.WriteLine(noise);
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string noise)
+        {
+            Console.WriteLine(noise);
+        }
+
+        public void MakeHonkNoise(string noise)
+        {
+            Console.WriteLine(noise);
+        }
     }
 
-    public void MakeHonkNoise(string noise)
-    {
-        Console.WriteLine(noise);
-    }
+
+
 }
-
-    
-
-
 
 
